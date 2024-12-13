67d8e36720c97158d7d08d970abe9e0551f660d2
Dhanalaxmi-Musku <135241955+Dhanalaxmi-Musku@users.noreply.github.com>
2024-12-13 20:38:27 +0530

    First commit

---
+++ src/BinarySearchTreeMain.java
@@ -0,0 +1,9 @@
+
+public class BinarySearchTreeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/MyBinarySearchTree.java
@@ -0,0 +1,11 @@
+
+public class MyBinarySearchTree<T extends Comparable<T>> {
+	 private MyBinaryNode<T> root;
+	    private int size;
+
+	    public MyBinarySearchTree() {
+	        this.root = null;
+	        this.size = 0;
+	    }
+
+}
